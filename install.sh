aa60e3ba332e4694963c8f39ec574a3637f7681f
nophagundol-git <57673759+nophagundol-git@users.noreply.github.com>
2022-03-26 19:16:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/bash
+null="> /dev/null 2>&1"
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+mkdir -p $HOME/.config/clash
+echo ""
+
+echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo ""
+
+echo -e $b"➤ "$w" prepare for installing dependencies ..."
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"clash"$w
+pkg install clash
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"unzip"$w
+pkg install unzip
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"micro"$w
+pkg install micro
+echo ""
+
+cd $HOME/ClashForTermux
+sleep 2
+
+echo -e $b"➤ "$w" coppying yacd-dashbord: "$g"yacd"$w
+cp -r -f yacd-gh-pages.zip $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying Indonesian.yaml $g"Indonesian"$w
+cp -r -f Indonesian.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying reject.yaml $g"reject"$w
+cp -r -f reject.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying streaming.yaml $g"streaming"$w
+cp -r -f streaming.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying sosmed.yaml: "$g"sosmed"$w
+cp -r -f sosmed.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
+cp -r -f config.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+cd $HOME/.config/clash
+echo -e $b"➤ "$w" extracting yacd-dashbord: "$g"yacd"$w
+unzip yacd-gh-pages.zip
+echo ""
+
+sleep 2
+rm -r -f yacd-gh-pages.zip
+sleep 2
+cd
+rm -r -f ClashForTermux
+clash
+clear
